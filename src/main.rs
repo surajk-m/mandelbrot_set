dffd73b3a6c32c29444f09c23a80f0ba25a57331
surajk-m <suraj410401@gmail.com>
2024-09-05 18:10:53 +0530

    use `MandelbrotConfig` struct

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,49 @@
 use image::{ImageBuffer, RgbImage};
 
 mod mandelbrot;
-use mandelbrot::generate_mandelbrot;
+use mandelbrot::{generate_mandelbrot, MandelbrotConfig};
 
-fn render_mandelbrot(
-    width: u32,
-    height: u32,
-    max_iter: u32,
-    center_x: f64,
-    center_y: f64,
-    zoom: f64,
-) {
-    let pixels = generate_mandelbrot(width, height, max_iter, center_x, center_y, zoom);
-    let img: RgbImage = ImageBuffer::from_raw(width, height, pixels).unwrap();
+fn render_mandelbrot(config: MandelbrotConfig) {
+    let pixels = generate_mandelbrot(&config);
+    let img: RgbImage = ImageBuffer::from_raw(config.width, config.height, pixels).unwrap();
     img.save("mandelbrot.png").unwrap();
 }
 
 fn main() {
-    // let width = 800;
-    // let height = 600;
-    // let max_iter = 1500;
+    // 4K resolution
+    let config = MandelbrotConfig {
+        width: 3840,
+        height: 2160,
+        max_iter: 5000,
+        center_x: -0.5,
+        center_y: 0.0,
+        zoom: 1.0,
+    };
 
-    let width = 3840; // 4K resolution width
-    let height = 2160; // 4K resolution height
-    let max_iter = 5000; // more iterations for ultra-fine details
+    // 8K resolution
+    // let config = MandelbrotConfig {
+    //     width: 7680,
+    //     height: 4320,
+    //     max_iter: 10000,
+    //     center_x: -0.5,
+    //     center_y: 0.0,
+    //     zoom: 1.0,
+    // };
 
-    // let width = 7680;       // 8K resolution width
-    // let height = 4320;      // 8K resolution height
-    // let max_iter = 10000;
-
-    // let width = 15360;      // 16K resolution width
-    // let height = 8640;      // 16K resolution height
-    // let max_iter = 10000;
-
-    // Center of the zoom and zoom level
-    let center_x = -0.5;
-    let center_y = 0.0;
-    let zoom = 1.0;
+    // 16K resolution
+    // let config = MandelbrotConfig {
+    //     width: 15360,
+    //     height: 8640,
+    //     max_iter: 10000,
+    //     center_x: -0.5,
+    //     center_y: 0.0,
+    //     zoom: 1.0,
+    // };
 
     // let center_x = -0.75;
     // let center_y = 0.1;
     // let zoom = 100.0; // 100x zoom into a specific area of the set
 
-    render_mandelbrot(width, height, max_iter, center_x, center_y, zoom);
+    render_mandelbrot(config);
     println!("Mandelbrot set generated and saved as 'mandelbrot.png'.");
 }
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -12,42 +12,53 @@ fn mandelbrot(c: Complex<f64>, max_iter: u32) -> u32 {
     max_iter
 }
 
-pub fn generate_mandelbrot(
-    width: u32,
-    height: u32,
-    max_iter: u32,
-    center_x: f64,
-    center_y: f64,
-    zoom: f64,
-) -> Vec<u8> {
-    let scale_x = 3.0 / (width as f64 * zoom);
-    let scale_y = 2.0 / (height as f64 * zoom);
+pub fn generate_color(ratio: f64) -> (u8, u8, u8) {
+    let red = (255.0 * (ratio).powf(0.5)) as u8;
+    let green = (255.0 * (1.0 - ratio).powf(2.0)) as u8;
+    let blue = (255.0 * (ratio * (1.0 - ratio)).powf(0.5)) as u8;
+    (red, green, blue)
+}
 
-    // Parallelize the processing of each row
-    let pixels: Vec<u8> = (0..height)
+pub struct MandelbrotConfig {
+    pub width: u32,
+    pub height: u32,
+    pub max_iter: u32,
+    pub center_x: f64,
+    pub center_y: f64,
+    pub zoom: f64,
+}
+
+pub fn generate_mandelbrot(config: &MandelbrotConfig) -> Vec<u8> {
+    let scale_x = 3.0 / (config.width as f64 * config.zoom);
+    let scale_y = 2.0 / (config.height as f64 * config.zoom);
+
+    // Collect the pixels in parallel
+    let pixels: Vec<u8> = (0..config.height)
         .into_par_iter()
         .flat_map(|y| {
-            (0..width)
-                .flat_map(move |x| {
-                    let cx = center_x + (x as f64 - width as f64 / 2.0) * scale_x;
-                    let cy = center_y + (y as f64 - height as f64 / 2.0) * scale_y;
+            // For RGB values
+            let mut row_pixels = Vec::with_capacity((config.width * 3) as usize);
+            for x in 0..config.width {
+                let cx = config.center_x + (x as f64 - config.width as f64 / 2.0) * scale_x;
+                let cy = config.center_y + (y as f64 - config.height as f64 / 2.0) * scale_y;
 
-                    let c = Complex::new(cx, cy);
-                    let m = mandelbrot(c, max_iter);
+                let c = Complex::new(cx, cy);
+                let m = mandelbrot(c, config.max_iter);
 
-                    let pixel = if m < max_iter {
-                        let ratio = m as f64 / max_iter as f64;
-                        let red = (255.0 * (ratio).powf(0.5)) as u8;
-                        let green = (255.0 * (1.0 - ratio).powf(2.0)) as u8;
-                        let blue = (255.0 * (ratio * (1.0 - ratio)).powf(0.5)) as u8;
-                        vec![red, green, blue]
-                    } else {
-                        // Black for points in the Mandelbrot set
-                        vec![0, 0, 0]
-                    };
-                    pixel
-                })
-                .collect::<Vec<u8>>()
+                if m < config.max_iter {
+                    let ratio = m as f64 / config.max_iter as f64;
+                    let (red, green, blue) = generate_color(ratio);
+                    row_pixels.push(red);
+                    row_pixels.push(green);
+                    row_pixels.push(blue);
+                } else {
+                    // Black for points in the Mandelbrot set
+                    row_pixels.push(0);
+                    row_pixels.push(0);
+                    row_pixels.push(0);
+                }
+            }
+            row_pixels
         })
         .collect();
 
